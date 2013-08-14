75fa3353a8de7a37eded2312aaab3f9a31890553
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-08-14 19:40:52 +0200

    Dirty workaround not shutting down when pressing Ctrl-C.

--- run.py
+++ run.py
@@ -7,9 +7,13 @@ sys.path.append(os.path.dirname(__file__))
 from derpbot import bot
 
 if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "You have to specify a configuration file!"
+		sys.exit(1)
 	
-	bot = bot.Bot("config.xml")
-	
-	bot.start()
-	raw_input("")
-	bot.stop()
+	bot = bot.Bot(sys.argv[1])
+	try:
+		bot.start()
+		raw_input("")
+	finally:
+		bot.stop()
