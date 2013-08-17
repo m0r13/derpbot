231f6edd17d7d7265fe13fab9c92a427a5922a71
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-08-17 18:45:13 +0200

    Updated fun plugin.

--- derpbot/plugins/fun.py
+++ derpbot/plugins/fun.py
@@ -2,8 +2,8 @@ from derpbot import plugin
 import random
 
 class FunPlugin(plugin.Plugin):
-    def __init__(self, bot, config):
-        super(FunPlugin, self).__init__(bot, config)
+    def __init__(self, *args, **kwargs):
+        super(FunPlugin, self).__init__(*args, **kwargs)
         
     def enable(self):
         pass
@@ -18,19 +18,19 @@ class FunPlugin(plugin.Plugin):
             else:
                 channel.send("indeed")
     
-    @plugin.command("slap .*",
+    @plugin.command("slap (.*)",
                     name="slap",
                     usage="slap <username>",
                     desc="Slaps another user.")
     def hello(self, channel, nick, match, message, args):
-        if len(args) > 1:
-            channel.send("%s slaps %s." % (nick, "".join(args[1:])))
+        channel.send("%s slaps %s." % (nick, match.group(1)))
 
-    @plugin.command("kree .*",
+    @plugin.command("kree (.*)",
                     name="kree",
                     usage="kree <username>",
                     desc="Jaffa Kree!")
     def kree(self, channel, nick, match, message, args):
-        if not self.bot.is_admin(nick) or len(args) < 2:
-            return
-        channel.sendto(args[1], "Jaffa Kree!")
+        if not self.bot.is_admin(nick):
+            channel.sendto(nick, "Sorry, I can't find any Naquadah in your blood.")
+        else:
+            channel.sendto(match.group(1), "Jaffa Kree!")
